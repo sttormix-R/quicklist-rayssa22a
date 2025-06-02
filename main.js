02f0e61695d1f0bf08b47f2ce612d25fc609644b
rayssa0433 <rayssa0433@gmail.com>
2025-06-02 04:13:02 +0000

    main js

---
+++ main.js
@@ -0,0 +1,17 @@
+const items = []
+
+function addItem(){
+    const itemName = document.querySelector("#item").value
+
+    // objeto
+    const item = {
+        name: itemName,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+    
+    console.log(items)
+}
